3a4b55f9c7c507c9bed7ac56a4bfe85cfc378b3f
(_S_K_) <tssathya26@gmail.com>
2025-08-21 00:19:48 +0530

    initial commit

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class MOVIE(models.Model):
+    poster = models.URLField(null=True, blank=True)
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20)
+    released_year = models.IntegerField(max_length=4)
+    rating = models.FloatField(default=0.0)
+    overview = models.TextField()
+    director = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
