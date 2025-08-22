e779109aabef1805ccf418477c25cdfa4d908f6d
(_S_K_) <tssathya26@gmail.com>
2025-08-22 23:54:19 +0530

    finally got the dataset working

--- ratings/models.py
+++ ratings/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class MOVIE(models.Model):
     poster = models.URLField(null=True, blank=True)
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=20)
-    released_year = models.IntegerField(max_length=4)
-    rating = models.FloatField(default=0.0)
+    released_year = models.IntegerField()
+    ratings = models.FloatField(default=0.0)
     overview = models.TextField()
     director = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
+
+class RATING(models.Model):
+    rating = models.IntegerField(choices=[(i , i)for i in range(1,6)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(MOVIE, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
+    
+    
--- ratings/views.py
+++ ratings/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 from .models import MOVIE
 # Create your views here.
 
+
+def home(request):
+    pass
+
+def movie_list_view(request):
+
+    movies = MOVIE.objects.all().order_by('ratings')
+
+    return render(request, 'ratings/list.html', {
+        'movies' : movies,
+        
+        })
