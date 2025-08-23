92f647bd6636c644129cf2f2fa31acf3234a655c
(_S_K_) <tssathya26@gmail.com>
2025-08-23 17:32:55 +0530

    adder sorting option

--- ratings/views.py
+++ ratings/views.py
@@ -8,9 +8,17 @@ def home(request):
     pass
 
 def movie_pages_view(request):
-    movies = MOVIE.objects.all()
-    paginator = Paginator(movies, 25)
+    movies = MOVIE.objects.all().order_by()
+    sort_by = request.GET.get('sort', 'title')
+    
+    if sort_by == 'genre':
+        movies = movies.order_by('genre')
+    elif sort_by == 'released_year':
+        movies = movies.order_by('released_year')
+    else:
+        movies = movies.order_by('title')
 
+    paginator = Paginator(movies, 25)
     page_number = request.GET.get('page')
     movies_per_page = paginator.get_page(page_number)
     return render(request, "ratings/list.html", {
